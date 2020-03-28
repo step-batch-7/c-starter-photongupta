3fa811813b28a938721727bf0e53ab26cf94c3ae
Rashmi Gupta <rashmi224141@gmail.com>
2020-03-28 16:34:43 +0530

    Added isOdd function

--- basicFunctions.c
+++ basicFunctions.c
@@ -1,14 +1,27 @@
 #include<stdio.h>
 
 int isEven(int);
+int isOdd(int);
 
 int isEven(int num){
   return num %2==0;
 }
 
+int isOdd(int num){
+  return !isEven(num);
+}
+
+
 int main(void){
-  int num;
+  
+  int num1;
   printf("Enter a number to check whether the number is even or not: ");
-  scanf("%d",&num);
-  printf("%s",isEven(num)?"even":"not even");
+  scanf("%d",&num1);
+  printf("%s \n",isEven(num1)?"even":"not even");
+
+  int num2;
+  printf("Enter a number to check whether the number is odd or not: ");
+  scanf("%d",&num2);
+  printf("%s \n",isOdd(num2)?"odd":"not odd");
+  
 }
