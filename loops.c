9320176828667203d47a274527a64f40755b32e0
Rashmi Gupta <rashmi224141@gmail.com>
2020-03-28 21:53:50 +0530

    Added fibo function in order to get the fibonaccai series

--- loops.c
+++ loops.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 int factorial(int);
+int fibo(int);
 
 int factorial(int num){
   if(num==0){
@@ -9,10 +10,32 @@ int factorial(int num){
   return factorial(num-1)*num;
 }
 
+int fibo(int term){
+  int lastTerm = 0;
+  int currentTerm = 1;
+  int nextTerm;
+
+  for (int i = 1; i <= term; i++){
+    printf("%d \n",lastTerm);
+    nextTerm = currentTerm + lastTerm;
+    lastTerm = currentTerm;
+    currentTerm = nextTerm;
+  }
+
+  return 0;
+}
+
 int main(void){
+
   int num;
   printf("Enter a number to get the factorial :");
   scanf("%d",&num);
   printf("factorial of %d is %d",num,factorial(num));
+
+   int num;
+   printf("Enter a term to get the fibonacci series :");
+   scanf("%d",&num);
+   fibo(num);
+
   return 0;
 }
